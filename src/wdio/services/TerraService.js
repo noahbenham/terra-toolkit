4e564c81ad9233fac9d579e80afa364f560522fa
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-04-03 14:13:19 -0500

    Started work on Clean up.

--- src/wdio/services/TerraService.js
+++ src/wdio/services/TerraService.js
@@ -1,4 +1,7 @@
 import chai from 'chai';
+import fse from 'fs-extra';
+import glob from 'glob';
+import path from 'path';
 import chaiMethods from './TerraCommands/chai-methods';
 import accessiblity from './TerraCommands/accessiblity';
 import visualRegression from './TerraCommands/visual-regression';
@@ -28,13 +31,40 @@ const getViewports = (...sizes) => {
   return viewportSizes.map(size => VIEWPORTS[size]);
 };
 
+// Update to use glob to grab the search patterns provided
+// const cleanUpScreenshots = (config) => {
+//   console.log("SPECS", config.specs);
+//   const screenshotSetup = (config.terra || {}).screenshotSetup || {};
+//   const snapshotDir = screenshotSetup.snapshotDir || '__snapshots__';
+//   const diffDir = screenshotSetup.diffDir || 'diff';
+//   const screenshotDir = screenshotSetup.screenshotDir || 'screenshot';
+//
+//   (config.specs).forEach((specPattern) => {
+//     glob.sync(specPattern).forEach((spec) => {
+//       const testDir = path.parse(spec).dir;
+//
+//       // still too coupled to our defualt config??
+//       const diffSnapshotDir = path.join(testDir, snapshotDir, diffDir);
+//       const screenSnapshotDir = path.join(testDir, snapshotDir, screenshotDir);
+//
+//       fse.removeSync(diffSnapshotDir);
+//       fse.removeSync(screenSnapshotDir);
+//     });
+//   });
+// };
+
 /**
 * Webdriver.io TerraService
-* Provides global access to chia, as well as custom chai assertions.
+* Provides global access to chai, as well as custom chai assertions.
 * Also provides access a global instance of the Terra object which
 * provides accessibliy and visual regression test steps.
 */
 export default class TerraService {
+  // eslint-disable-next-line class-methods-use-this
+  // async onPrepare(config) {
+  //   cleanUpScreenshots(config);
+  // }
+
   // eslint-disable-next-line class-methods-use-this
   before() {
     chai.config.showDiff = false;
